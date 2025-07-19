a9cfb035731a19e5d4acfcd2815a667a964d62f2
vishwaspatil143 <vishwasfrontechpatil@gmail.com>
2025-07-19 11:28:41 +0000

    Remove unused code

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,9 +2,7 @@ class StringCalculator
   def self.add(input_string)
     if input_string.empty?
       0
-    else      
-      # sum_of_numbers = input_string.gsub("\n", ",").split(',').map(&:to_i)
-      # sum_of_numbers.sum
+    else
       delimiter = /,|\n/
       numbers_string = input_string
 
